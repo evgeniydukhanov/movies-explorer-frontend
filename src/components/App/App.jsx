d8e99d2c63a6dc8f9f4bb2c0ebde87113ed2c011
evgeniydukhanov <duhanoves@itwis.org>
2022-06-01 21:07:20 +0700

    added header, css files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,21 +6,22 @@ import SavedMovies from '../SavedMovies/SavedMovies';
 import Profile from '../Profile/Profile';
 import Login from '../Login/Login';
 import Register from '../Register/Register';
+import Header from '../Header/Header';
+import './App.css';
 
 function App() {
   return (
-    <div className='body'>
       <div className='page'>
-        <Switch>
+        <Header />
+        {/* <Switch>
           <Route exact path='/' component={Main}></Route>
           <Route path='/movies' component={Movies}></Route>
           <Route path='/saved-movies' component={SavedMovies}></Route>
           <Route path='/profile' component={Profile}></Route>
           <Route path='/signin' component={Login}></Route>
           <Route path='/signup' component={Register}></Route>
-        </Switch>
+        </Switch> */}
       </div>
-    </div>
   );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../../images/logo.svg';
+import {Link} from 'react-router-dom';
+
+function Header(){
+    return(
+        <header className='header'>
+            <Link to='/'>
+                <img src={logo} alt ='Логотип'></img>
+            </Link>
+            {/* <Link to='/sign-up'>
+                Регистрация
+            </Link>
+            <Link to='/sign-in'>
+                Войти
+            </Link> */}
+        </header>
+    )
+}
+
+export default Header;
