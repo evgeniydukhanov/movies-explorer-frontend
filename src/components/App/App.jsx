4bf6539266cc17322d2ef36318d23fe1c0586cbe
evgeniydukhanov <duhanoves@itwis.org>
2022-06-06 20:28:09 +0700

    added login & register screen

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,28 +4,31 @@ import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
 import SavedMovies from '../SavedMovies/SavedMovies';
 import Profile from '../Profile/Profile';
-import Login from '../Login/Login';
-import Register from '../Register/Register';
+import Login from '../Auth/Login';
+import Register from '../Auth/Register';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import './App.css';
 
-function App() {
-  const [loggedIn,setLoggedIn] = useState(true); 
+function App(props) {
+  const [loggedIn, setLoggedIn] = useState(true);
   return (
-      <div className='page'>
-        <Header loggedIn={loggedIn}/>
-        <Switch>
-          <Route exact path='/' component={Main}></Route>
-          <Route path='/movies' component={Movies}></Route>
-          {/* <Route path='/saved-movies' component={SavedMovies}></Route>
+    <div className='page'>
+      {/* <Header loggedIn={loggedIn} /> */}
+      <Switch>
+        <Route exact path='/' component={Main}></Route>
+        <Route path='/movies' component={Movies}></Route>
+        {/* <Route path='/saved-movies' component={SavedMovies}></Route>
           <Route path='/profile' component={Profile}></Route> */}
-          <Route path='/sign-in' component={Login}></Route>
-          <Route path='/sign-up' component={Register}></Route>
-          
-        </Switch>
-        <Footer/>
-      </div>
+        <Route path='/sign-in' component={Login}></Route>
+        <Route
+          path='/sign-up'
+          component={Register}
+          buttonText='Зарегистрироваться'
+        ></Route>
+      </Switch>
+      {/* <Footer /> */}
+    </div>
   );
 }
 
---
+++ src/components/Auth/Register.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from '../../images/logo.svg';
+
+function Register(props) {
+  return (
+    <section className="auth">
+      <img className='header__logo header__logo_auth' src={logo} alt='Логотип'></img>
+      <h2 className="auth__heading">Добро пожаловать!</h2>
+      <form className="auth__form">
+        <p className='auth__input-name'>Имя</p>
+      <input
+          className="auth__input"
+          required
+          type="text"
+          name="name"
+        />
+        <p className='auth__input-name'>E-mail</p>
+        <input
+          className="auth__input"
+          required
+          type="text"
+          name="email"
+        />
+        <p className='auth__input-name'>Пароль</p>
+        <input
+          className="auth__input"
+          required
+          type="password"
+          name="password"
+          minLength="8"
+          maxLength="16"
+        />
+        <button className="auth__submitBtn" type="submit">
+          Зарегистрироваться
+        </button>
+      </form>
+      <div className='auth__redirect'>
+      <p className='auth__redirect-text'>Уже зарегистрированы?</p>
+      <Link to="/sign-in" className="auth__redirect-link">
+         Войти
+      </Link>
+      </div>
+    </section>
+  );
+}
+
+export default Register;
