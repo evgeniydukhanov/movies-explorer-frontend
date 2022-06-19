b4d0c123f29dd2f02c54c5b11711e6ba4f33412c
evgeniydukhanov <duhanoves@itwis.org>
2022-06-19 23:17:39 +0700

    added preloader

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -4,27 +4,38 @@ import { Route } from 'react-router-dom';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import moviesApi from '../../utils/MoviesApi';
+import Preloader from '../Preloader/Preloader';
+
 function MoviesCardList() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
+    setIsLoading(true);
     moviesApi
       .getMovies()
       .then((movies) => {
         setMovies(movies);
+        setIsLoading(false);
       })
       .catch((err) => `Не удалось получить карточки с сервера : ${err}`);
   }, [movies.length]);
 
   return (
     <div className='cards'>
-      <div className='card__list'>
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} {...movie} />
-        ))}
-      </div>
-      <Route path='/movies'>
-        <button className='cards__more-films'>Еще</button>
-      </Route>
+      {isLoading ? (
+        <Preloader />
+      ) : (
+        <>
+          <div className='card__list'>
+            {movies.map((movie) => (
+              <MovieCard key={movie.id} {...movie} />
+            ))}
+          </div>
+          <Route path='/movies'>
+            <button className='cards__more-films'>Еще</button>
+          </Route>
+        </>
+      )}
     </div>
   );
 }
